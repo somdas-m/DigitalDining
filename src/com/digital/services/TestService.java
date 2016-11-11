233d706b15bbcfe003f0308129cc1b78374481c0
somdas-m <deathisonce@gmail.com>
2016-11-12 01:37:49 +0530

    To test db connection

--- src/com/digital/services/TestService.java
+++ src/com/digital/services/TestService.java
@@ -2,6 +2,7 @@ package com.digital.services;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import com.digital.pojo.Item;
 
 import com.digital.util.HibernateUtil;
 
@@ -9,9 +10,23 @@ import com.digital.util.HibernateUtil;
 public class TestService {
 	@GET
 	@Path("/test")
-	public String testWorking(){
+	public HashMap<String, String> testWorking(){
+		Item item = new Item();
+		item.setItemNumber("S101");
+		item.setItemName("TEST_ITEM");
+		item.setItemPrice(1.1);
+		item.setCategory("TEST_CAT");
+		System.out.println(item);
 		System.out.println("Gonna start test");
-		HibernateUtil.getSessionFactory();
-		return "Test Successfull";
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.saveOrUpdate(item);
+		session.getTransaction().commit();
+		session.close();
+		
+		HashMap<String, String> result = new HashMap<>();
+		result.put("status", "success");
+		result.put("msg", "Job Application Submitted");
+		return result;
 	}
 }
