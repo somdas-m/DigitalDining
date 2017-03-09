81f76a67ba8114ff751ec47bd6e56b234ac3a86e
somdas-m <deathisonce@gmail.com>
2017-03-09 14:49:31 +0530

    introduced constants

---
+++ src/com/digital/constants/expenseManagerConstants.java
@@ -0,0 +1,16 @@
+package com.digital.constants;
+
+public class expenseManagerConstants {
+	public static String T_ID = "transactionID";
+	public static String T_Date = "transactionDate";
+	public static String T_Particulars = "transactionParticulars";
+	public static String T_Amount = "transactionAmount";
+	public static String T_Category = "transactionCategory";
+	public static String T_isDebit = "transactionBorrowed";
+	public static String T_DebitedFrom = "transactionDebitedFrom";
+	public static String T_DebitSettled = "transactionDebitSettled";
+	public static String T_isCredit = "transactionCredited";
+	public static String T_CreditTo = "transactionCreditedTo";
+	public static String T_CreditSettled = "transactionCreditSettled";
+	public static String T_Timestamp = "transactionTimestamp";
+}
