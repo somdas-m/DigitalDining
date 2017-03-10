7dc7fff7dc1487e2e3aad46689de8dfc4e532803
somdas-m <deathisonce@gmail.com>
2017-03-10 15:19:04 +0530

    Update TestService.java

--- src/com/digital/services/TestService.java
+++ src/com/digital/services/TestService.java
@@ -56,7 +56,7 @@ public class TestService {
 			transaction.setCreditSettled(jsonObject.getJSONObject(expenseManagerConstants.T_CreditSettled).toString());
 			transaction.setTimestamp(jsonObject.getJSONObject(expenseManagerConstants.T_Timestamp).toString());*/
 			//new Date(jsonObject.optString(expenseManagerConstants.T_Date))
-			Transaction transaction = new Transaction(,jsonObject.optString(null,expenseManagerConstants.T_Particulars),jsonObject.optString(expenseManagerConstants.T_Amount),jsonObject.optString(expenseManagerConstants.T_Category),jsonObject.optString(expenseManagerConstants.T_isDebit),jsonObject.optString(expenseManagerConstants.T_DebitedFrom),jsonObject.optString(expenseManagerConstants.T_DebitSettled),jsonObject.optString(expenseManagerConstants.T_isCredit),jsonObject.optString(expenseManagerConstants.T_CreditTo),jsonObject.optString(expenseManagerConstants.T_CreditSettled),jsonObject.optString(expenseManagerConstants.T_Timestamp));
+			Transaction transaction = new Transaction(null,jsonObject.optString(expenseManagerConstants.T_Particulars),jsonObject.optString(expenseManagerConstants.T_Amount),jsonObject.optString(expenseManagerConstants.T_Category),jsonObject.optString(expenseManagerConstants.T_isDebit),jsonObject.optString(expenseManagerConstants.T_DebitedFrom),jsonObject.optString(expenseManagerConstants.T_DebitSettled),jsonObject.optString(expenseManagerConstants.T_isCredit),jsonObject.optString(expenseManagerConstants.T_CreditTo),jsonObject.optString(expenseManagerConstants.T_CreditSettled),jsonObject.optString(expenseManagerConstants.T_Timestamp));
 			return transaction.toString();
 			//return DatabaseServices.insertToDB(transaction);
 		 }catch(Exception e){
