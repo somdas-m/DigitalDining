48206e9136ae6760032b61d5a46e4a856cae014e
somdas-m <deathisonce@gmail.com>
2017-03-10 20:17:26 +0530

    Update TestService.java

--- src/com/digital/services/TestService.java
+++ src/com/digital/services/TestService.java
@@ -55,7 +55,7 @@ public class TestService {
 			transaction.setCreditedTo(jsonObject.getJSONObject(expenseManagerConstants.T_CreditTo).toString());
 			transaction.setCreditSettled(jsonObject.getJSONObject(expenseManagerConstants.T_CreditSettled).toString());
 			transaction.setTimestamp(jsonObject.getJSONObject(expenseManagerConstants.T_Timestamp).toString());*/
-			Date dat = new Date(jsonObject.optString(expenseManagerConstants.T_Date))
+			Date dat = new Date(jsonObject.optString(expenseManagerConstants.T_Date));
 			Transaction transaction = new Transaction(dat,jsonObject.optString(expenseManagerConstants.T_Particulars),jsonObject.optString(expenseManagerConstants.T_Amount),jsonObject.optString(expenseManagerConstants.T_Category),jsonObject.optString(expenseManagerConstants.T_isDebit),jsonObject.optString(expenseManagerConstants.T_DebitedFrom),jsonObject.optString(expenseManagerConstants.T_DebitSettled),jsonObject.optString(expenseManagerConstants.T_isCredit),jsonObject.optString(expenseManagerConstants.T_CreditTo),jsonObject.optString(expenseManagerConstants.T_CreditSettled),jsonObject.optString(expenseManagerConstants.T_Timestamp));
 			return transaction.toString();
 			//return DatabaseServices.insertToDB(transaction);
